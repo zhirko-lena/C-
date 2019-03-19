c00b2d46b3116cee89e85923d52851e70db4e254
Lena Zhirko <lenazhirko@step.itstep.rv.ua>
2019-03-19 14:56:39 +0200

    New CW

---
+++ Class Works/LC-CW-19-03-2019/LC-CW-19-03-2019/Source.cpp
@@ -0,0 +1,140 @@
+#include <iostream>
+#include <ctime>
+# include <string>
+using namespace std;
+
+//int strlength(char *str) {
+//	int  counter = 0;
+//	while (str[counter] != '\0') {
+//		counter++;
+//	}
+//	return counter;
+//}
+//
+//int strlengthA(char *str) {
+//	int  counter1 = 0;
+//	int  counter2 = 0;
+//	for (int i = 0; str[counter1] != '\0'; i++)
+//	while (str[counter] != '\0') {
+//	counter++;
+//	}
+//	return counter;
+//	}
+//
+//int strlengthO(char *str) {
+//	int  counter = 0;
+//	while(str[counter] != '\0') {
+//		
+//		counter++;
+//	}
+//	return counter;
+//}
+//
+//int main() {
+//	/*string str = "hello";
+//	string str1 = "world";
+//	string res = str + " " + str1 + " " + "-my text";
+//	cout << res << endl;
+//	*/
+//		char str[255];
+//		cin >> str;
+//		
+//	
+//	cout << strlength(str) << endl;
+//
+//	char str1[255]/*= "hello"*/;
+//	char str2[255] /*= "world"*/;
+//	cin >> str1 >> str2;
+//	strcat_s(str1, str2);
+//	cout << str1 << endl;
+//
+//	/*char str[255];
+//	cin >> str;
+//
+//	cout << strlengthA(str) << endl;
+//	cout << strlengthO(str) << endl;*/
+//
+//
+//
+//
+//
+//	system("pause");
+//	return 0;
+//}
+
+//void strLength(char *text, int &latCounter, int &numCounter) {
+//
+//	for (int i = 0; i < strlen(text); i++) {
+//		if (text[latCounter] > 64 && text[latCounter] < 91 || text[latCounter]>96 && text[latCounter] < 123) {
+//			latCounter++;
+//		}
+//		else if (text[numCounter] > 47 && text[numCounter] < 58) {
+//			numCounter++;
+//		}
+//	}
+//
+//
+//}
+//
+//
+////void strLengthNum(char *text, int &numCounter) {
+////
+////	for (int i = 0; i < strlen(text); i++) {
+////		if (text[numCounter] > 47 && text[numCounter] < 58) {
+////			numCounter++;
+////		}
+////	}
+////
+////
+////}
+//
+//
+//
+//int main() {
+//
+//	char text[255];
+//	cin >> text;
+//	int latCounter = 0;
+//	int numCounter = 0;
+//
+//	strLength(text, latCounter, numCounter);
+//	cout << "latin symbols:" << latCounter << endl;
+//
+//	/*strLengthNum(text, numCounter);*/
+//	cout << "numbers:" << numCounter << endl;
+//
+//
+//
+//	system("pause");
+//	return 0;
+//}
+
+int main() {
+
+
+
+
+
+
+
+
+
+	system("pause");
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
